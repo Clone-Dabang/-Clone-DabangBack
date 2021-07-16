b2f9741328127eef04ffef23a12cb56eb6090c2b
Code-Angler <xkqkrtk@naver.com>
2021-07-16 16:59:17 +0900

    iniinitialize commit

---
+++ src/main/java/com/project/dabang/DabangApplication.java
@@ -0,0 +1,13 @@
+package com.project.dabang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DabangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DabangApplication.class, args);
+    }
+
+}
